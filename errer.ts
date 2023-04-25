d7e2c4686c3ad9b42393a10996fb5a9fb6ffb5d5
ivan783 <73236739+ivan783@users.noreply.github.com>
2023-04-25 00:38:56 -0400

    Update errer.ts
    rtyrtytryrtyrtyrtyrty

--- errer.ts
+++ errer.ts
@@ -42,3 +42,47 @@ src/app/Services/qr/models/RequestSucursal.ts:8:3
                 return Result<GetRegBranchQRResponse>.SetError("Error al guardar branch");
             }
         }
+public async Task<Result<List<GetRegBranchQRResponse>>> SaveRegBranch(RegRequestBranchDto Dto)
+{
+    try
+    {
+        var appUserId = configuration.GetSection("Connectors").GetSection("ApiQR")["AppUserId"];
+        var publicToken = configuration.GetSection("Connectors").GetSection("ApiQR")["PublicToken"];
+        var channel = configuration.GetSection("Connectors").GetSection("ApiQR")["Channel"];
+        var url = configuration.GetSection("Connectors").GetSection("ApiQR")["AddresRegBusiness"];
+
+        var responses = new List<GetRegBranchQRResponse>();
+        foreach (var item in Dto.RequestSucursal)
+        {
+            var request = new GetRegBranchQRRequest
+            {
+                PublicToken = publicToken,
+                AppUserId = appUserId,
+                Channel = channel,
+                BusinessCode = item.BusinessCode,
+                BranchName = item.BranchName,
+                City = item.City,
+                UserId = item.UserId
+            };
+
+            var aux = JsonConvert.SerializeObject(request);
+            logger.LogError($"Request: {aux}");
+            var response = await this.manager.SaveRegBranchQR(request, url);
+            responses.Add(response);
+
+            if (response.State == "00")
+            {
+                int branchQRPaymentId =
+                    SaveBranchQRPayment(0, item.BusinesId, response.Data.BranchCode.ToString(), request.BranchName, "Not", "Not",
+                                    "Migra", "", DateTime.Now, DateTime.Now, false, "Not", item.UserId, item.City);
+            }
+        }
+
+        return Result<List<GetRegBranchQRResponse>>.SetOk(responses);
+    }
+    catch (Exception ex)
+    {
+        logger.LogError($"Error al guardar Branch: {ex.Message}");
+        return Result<List<GetRegBranchQRResponse>>.SetError("Error al guardar branch");
+    }
+}
